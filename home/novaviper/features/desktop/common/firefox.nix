f79665c1dbe4a1e5f08e7ae2a4acf535c45f7015
NovaViper <coder.nova99@mailbox.org>
2024-06-18 14:05:35 -0500

    fix(home/firefox): fix NixOS Wiki search

--- home/novaviper/features/desktop/common/firefox.nix
+++ home/novaviper/features/desktop/common/firefox.nix
@@ -74,7 +74,7 @@
             definedAliases = ["@no"];
           };
           "NixOS Wiki" = {
-            urls = [{template = "https://wiki.nixos.org/index.php?search={searchTerms}";}];
+            urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
             iconUpdateURL = "https://wiki.nixos.org/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = ["@nw"];
