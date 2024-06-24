31b8137c300d17f814f8a6a09d3367bab54eee0e
NovaViper <coder.nova99@mailbox.org>
2024-06-23 23:43:42 -0500

    Rekey secrets and make users immutable, rekey hosts

--- hosts/common/users/novaviper/default.nix
+++ hosts/common/users/novaviper/default.nix
@@ -8,14 +8,12 @@
   ageSecretsPath = path: "${inputs.self}/secrets/${path}";
   ifTheyExist = groups:
     builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+  agenixHashedPasswordFile = lib.optionalString (lib.hasAttr "agenix" inputs) config.age.secrets."${config.variables.username}-password".path;
 in {
   # Special Variables
   variables.username = "novaviper";
 
-  # Secrets
-  age.secrets.novaviper-password.file = ageSecretsPath "novaviper/pass.age";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
   users.users.novaviper = {
     isNormalUser = true;
     shell = pkgs.zsh;
@@ -31,9 +29,12 @@ in {
         "git"
         "gamemode"
       ];
-    hashedPasswordFile = config.age.secrets.novaviper-password.path;
+    hashedPasswordFile = agenixHashedPasswordFile;
     packages = with pkgs; [home-manager];
   };
+
+  # Secrets
+  age.secrets."${config.variables.username}-password".file = ageSecretsPath "${config.variables.username}/passwd.age";
 
   # Import Home-Manager config for host
   home-manager.users.novaviper =
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,12 +4,12 @@ let
 
   users = {
     novaviper = [
-      "age1yubikey1q023pgeugaxany07tdc8vptwyc3jlh9rzlwq33a7gzndhlfe848jv8mtrvg" #yk5
+      "age1yubikey1q023pgeugaxany07tdc8vptwyc3jlh9rzlwq33a7gzndhlfe848jv8mtrvg" #yubikey 5
     ];
   };
   systems = {
-    #ryzennova = "";
-    yoganova = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ03MhTAi6x4doPmJpKCf445S72eRbTftyCcfR/MDn2A root@yoganova";
+    ryzennova = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqV/Sx++yIUmgLCLuO6YNrT2Qq/iJWbmpMQtSFwgSdF root@ryzennova";
+    yoganova = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiZ0B3Lvn1lD7UOTCtlX0q2yfJ4FpvwI4XWSmjwxK4R root@yoganova";
   };
 
   allUsers = lib.flatten (builtins.attrValues users);
@@ -19,7 +19,7 @@ in {
   # "tailscale.age".publicKeys = allUsers ++ allSystems;
 
   # User Pass
-  "novaviper/pass.age".publicKeys = users.novaviper ++ allSystems;
+  "novaviper/passwd.age".publicKeys = users.novaviper ++ allSystems;
 
   # Home-Manager User Secrets
   "novaviper/borg.age".publicKeys = users.novaviper;
