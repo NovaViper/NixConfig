a5142413a94e6c37e232d9078ae062b9ff1f28c5
NovaViper <coder.nova99@mailbox.org>
2024-09-23 16:07:56 -0500

    Add deadnix and statix to checks

--- checks/default.nix
+++ checks/default.nix
@@ -46,6 +46,18 @@
       };
 
       alejandra.enable = true;
+      deadnix = {
+        enable = true;
+        settings.edit = true;
+      };
+
+      statix = {
+        enable = true;
+        settings.ignore = [
+          ".direnv"
+          "**/hardware-configuration.nix"
+        ];
+      };
 
       shfmt = {
         enable = true;
