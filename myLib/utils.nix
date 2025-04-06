bc9b6aa5961c7a0f6e7c15bd649249a4ce0b8b09
NovaViper <coder.nova99@mailbox.org>
2025-04-05 21:48:05 -0500

    refactor: remove deprecated utility code

--- myLib/utils.nix
+++ myLib/utils.nix
@@ -4,36 +4,6 @@
   ...
 }: let
   exports = {
-    # Enable all modules in the list elems
-    enable = elems:
-      builtins.listToAttrs (map (name: {
-          inherit name;
-          value.enable = true;
-        })
-        elems);
-
-    # Disable all modules in the list elems
-    disable = elems:
-      builtins.listToAttrs (map (name: {
-          inherit name;
-          value.enable = false;
-        })
-        elems);
-
-    # Conditionally enable/disable all modules in the list elems
-    enableIf = cond: elems:
-      if cond
-      then (exports.enable elems)
-      else (exports.disable elems);
-
-    importFromPath = {
-      source,
-      paths,
-    }: let
-      importThings = builtins.map (d: source + "/${d}") paths;
-    in
-      importThings;
-
     # List everything in a given directory (dir)
     filesInDir = dir: lib.filesystem.listFilesRecursive dir;
 
@@ -87,72 +57,6 @@
         (lib.optionalString (signature != null) '':sig         "${signature}"'')
       ])})
     '';
-
-    # DEPRECATED Remove this since now we're using slimports
-    # Concatinatinates all file paths in a given directory into one list.
-    # It recurses through subdirectories. If it detects a default.nix, only that
-    # file will be considered.
-    concatImports = {
-      path ? null,
-      paths ? [],
-      include ? [],
-      exclude ? [],
-      recursive ? true,
-      filterDefault ? true,
-    }:
-      with lib;
-      with fileset; let
-        excludedFiles = filter pathIsRegularFile exclude;
-        excludedDirs = filter pathIsDirectory exclude;
-        isExcluded = path:
-          if elem path excludedFiles
-          then true
-          else
-            (filter (excludedDir: lib.path.hasPrefix excludedDir path)
-              excludedDirs)
-            != [];
-
-        myFiles = unique ((filter (file:
-            pathIsRegularFile file
-            && hasSuffix ".nix" (builtins.toString file)
-            && !isExcluded file) (concatMap (_path:
-              if recursive
-              then toList _path
-              else
-                mapAttrsToList (name: type:
-                  _path
-                  + (
-                    if type == "directory"
-                    then "/${name}/default.nix"
-                    else "/${name}"
-                  )) (builtins.readDir _path))
-            (unique (
-              if path == null
-              then paths
-              else [path] ++ paths
-            ))))
-          ++ (
-            if recursive
-            then concatMap toList (unique include)
-            else unique include
-          ));
-
-        dirOfFile = builtins.map (file: builtins.dirOf file) myFiles;
-
-        dirsWithDefaultNix =
-          builtins.filter (dir: builtins.elem dir dirOfFile)
-          (builtins.map (file: builtins.dirOf file) (builtins.filter (file:
-            builtins.match "default.nix" (builtins.baseNameOf file) != null)
-          myFiles));
-
-        filteredFiles = builtins.filter (file:
-          ! builtins.elem (builtins.dirOf file) dirsWithDefaultNix
-          || builtins.match "default.nix" (builtins.baseNameOf file) != null)
-        myFiles;
-      in
-        if filterDefault
-        then filteredFiles
-        else myFiles;
   };
 in
   exports
