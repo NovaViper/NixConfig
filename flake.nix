153afcd14815cac14b8b1c9ae02c9e02d86df8e0
NovaViper <coder.nova99@mailbox.org>
2025-05-01 14:49:12 -0500

    flake: split outputs into their own file

--- flake.nix
+++ flake.nix
@@ -13,6 +13,8 @@
     ];
   };
 
+  outputs = args: import ./outputs.nix args;
+
   inputs = {
     # Core dependencies
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
@@ -50,6 +52,7 @@
       url = "github:llakala/menu";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    #######
 
     # Extras
     stylix = {
@@ -81,6 +84,9 @@
       url = "github:MarceColl/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    #######
+
+    # Personal Repos
     wallpapers = {
       url = "github:NovaViper/Wallpapers";
       flake = false;
@@ -91,97 +97,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.systems.follows = "systems";
     };
+    #######
+
     # Nixpkgs PRs
     # TODO: https://github.com/NixOS/nixpkgs/pull/216245
     nixpkgs-howdy.url = "github:fufexan/nixpkgs/howdy";
+    #######
   };
-
-  # These are all just outputs for the flake
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      inherit (nixpkgs) lib;
-
-      # Overlay my custom lib and home-manager lib onto the default nixpkgs lib
-      myLib = import ./myLib { inherit inputs self; };
-
-      # Flake evaluation tests and checks entrypoint
-      # Available through 'nix flake check'
-      checks = myLib.forEachSystem (pkgs: import ./checks { inherit inputs pkgs; });
-
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations =
-        # Run mkHost for each nixosConfiguration, with key passed as hostname
-        builtins.mapAttrs myLib.mkHost {
-          # Main desktop
-          ryzennova = {
-            primaryUser = "novaviper";
-            system = "x86_64-linux";
-            stateVersion = "24.11";
-          };
-
-          # Personal laptop
-          yoganova = {
-            primaryUser = "novaviper";
-            system = "x86_64-linux";
-            stateVersion = "24.11";
-          };
-
-          # Live-image installer
-          installer = {
-            primaryUser = "nixos";
-            system = "x86_64-linux";
-            stateVersion = "25.05";
-          };
-        };
-    in
-    {
-      # Just inherit everything we made in the let statement
-      inherit myLib checks nixosConfigurations;
-
-      # Reusable nixos modules you might want to export
-      # These are usually stuff you would upstream into nixpkgs
-      nixosModules.default = myLib.slimports { paths = lib.singleton ./extra/nixosModules; };
-
-      # Reusable home-manager modules you might want to export
-      # These are usually stuff you would upstream into home-manager
-      homeModules.default = myLib.slimports { paths = lib.singleton ./extra/homeModules; };
-
-      # Your custom packages and modifications, exported as overlays output
-      overlays = import ./overlays { inherit self; };
-
-      # Your custom packages
-      # Acessible through 'nix build', 'nix shell', etc
-      packages = myLib.forEachSystem (
-        pkgs:
-        (lib.packagesFromDirectoryRecursive {
-          inherit (pkgs) callPackage;
-          directory = ./pkgs/common;
-        })
-      );
-
-      # Devshell for bootstrapping
-      # Acessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = myLib.forEachSystem (pkgs: import ./shell.nix { inherit pkgs checks; });
-
-      # Formatter for your nix files, available through 'nix fmt'
-      # Other options beside 'alejandra' include 'nixpkgs-fmt'
-      formatter = myLib.forEachSystem (pkgs: pkgs.nixfmt-tree);
-
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations =
-        # Run mkHome for each homeConfiguration, with key passed as host
-        builtins.mapAttrs myLib.mkHome {
-          "novaviper@ryzennova" = { inherit nixosConfigurations; };
-          "novaviper@yoganova" = { inherit nixosConfigurations; };
-          "nixos@installer" = { inherit nixosConfigurations; };
-        };
-    };
 }
---
+++ outputs.nix
@@ -0,0 +1,81 @@
+{ self, nixpkgs, ... }@inputs:
+let
+  inherit (nixpkgs) lib;
+
+  # Overlay my custom lib and home-manager lib onto the default nixpkgs lib
+  myLib = import ./myLib { inherit inputs self; };
+
+  # Flake evaluation tests and checks entrypoint
+  # Available through 'nix flake check'
+  checks = myLib.forEachSystem (pkgs: import ./checks { inherit inputs pkgs; });
+
+  # NixOS configuration entrypoint
+  # Available through 'nixos-rebuild --flake .#your-hostname'
+  nixosConfigurations =
+    # Run mkHost for each nixosConfiguration, with key passed as hostname
+    builtins.mapAttrs myLib.mkHost {
+      # Main desktop
+      ryzennova = {
+        primaryUser = "novaviper";
+        system = "x86_64-linux";
+        stateVersion = "24.11";
+      };
+
+      # Personal laptop
+      yoganova = {
+        primaryUser = "novaviper";
+        system = "x86_64-linux";
+        stateVersion = "24.11";
+      };
+
+      # Live-image installer
+      installer = {
+        primaryUser = "nixos";
+        system = "x86_64-linux";
+        stateVersion = "25.05";
+      };
+    };
+in
+{
+  # Just inherit everything we made in the let statement
+  inherit myLib checks nixosConfigurations;
+
+  # Reusable nixos modules you might want to export
+  # These are usually stuff you would upstream into nixpkgs
+  nixosModules.default = myLib.slimports { paths = lib.singleton ./extra/nixosModules; };
+
+  # Reusable home-manager modules you might want to export
+  # These are usually stuff you would upstream into home-manager
+  homeModules.default = myLib.slimports { paths = lib.singleton ./extra/homeModules; };
+
+  # Your custom packages and modifications, exported as overlays output
+  overlays = import ./overlays { inherit self; };
+
+  # Your custom packages
+  # Acessible through 'nix build', 'nix shell', etc
+  packages = myLib.forEachSystem (
+    pkgs:
+    (lib.packagesFromDirectoryRecursive {
+      inherit (pkgs) callPackage;
+      directory = ./pkgs/common;
+    })
+  );
+
+  # Devshell for bootstrapping
+  # Acessible through 'nix develop' or 'nix-shell' (legacy)
+  devShells = myLib.forEachSystem (pkgs: import ./shell.nix { inherit pkgs checks; });
+
+  # Formatter for your nix files, available through 'nix fmt'
+  # Other options beside 'alejandra' include 'nixpkgs-fmt'
+  formatter = myLib.forEachSystem (pkgs: pkgs.nixfmt-tree);
+
+  # Standalone home-manager configuration entrypoint
+  # Available through 'home-manager --flake .#your-username@your-hostname'
+  homeConfigurations =
+    # Run mkHome for each homeConfiguration, with key passed as host
+    builtins.mapAttrs myLib.mkHome {
+      "novaviper@ryzennova" = { inherit nixosConfigurations; };
+      "novaviper@yoganova" = { inherit nixosConfigurations; };
+      "nixos@installer" = { inherit nixosConfigurations; };
+    };
+}
