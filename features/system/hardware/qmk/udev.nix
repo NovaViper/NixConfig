bc2700b72e82820af9fc541e2cc07f910122c50d
NovaViper <coder.nova99@mailbox.org>
2025-05-16 00:44:12 -0500

    chore: rekey ryzennova (and disable udev rules for qmk)

--- features/system/hardware/qmk/udev.nix
+++ features/system/hardware/qmk/udev.nix
@@ -7,8 +7,9 @@
 {
   services.udev.packages = [ pkgs.via ];
 
-  services.udev.extraRules = ''
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl
-  '';
+  # FIXME: Currently broken
+  # services.udev.extraRules = ''
+  #   KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  #   KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl
+  # '';
 }
