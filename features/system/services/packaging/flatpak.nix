8b5ba55b5dad795e94fb71c9e87764072ab14547
NovaViper <coder.nova99@mailbox.org>
2025-05-26 11:09:28 -0500

    flatpak: switch to systemd service for initalization

--- features/system/services/packaging/flatpak.nix
+++ features/system/services/packaging/flatpak.nix
@@ -7,21 +7,11 @@
   fonts.fontDir.enable = true;
 
   # Automatically configure it
-  /*
-      systemd.services.configure-flathub-repo = {
-      wantedBy = ["multi-user.target"];
-      path = [pkgs.flatpak];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
-  */
-
-  environment.etc = {
-    "flatpak/remotes.d/flathub.flatpakrepo".source = pkgs.fetchurl {
-      url = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      # Let this run once and you will get the hash as an error.
-      hash = "sha256-M3HdJQ5h2eFjNjAHP+/aFTzUQm9y9K+gwzc64uj+oDo=";
-    };
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
   };
 }
