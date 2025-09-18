a8ac587369d732a9f12bf76f496bec276f19cfe4
NovaViper <coder.nova99@mailbox.org>
2025-09-18 16:47:14 -0500

    ghostty: Fix systemd service and replace the one it makes on install

--- features/programs/ghostty/ghostty.nix
+++ features/programs/ghostty/ghostty.nix
@@ -59,20 +59,28 @@ in
     ];
   };
 
-  hm.systemd.user.services.ghostty-daemon = {
+  hm.systemd.user.services."app-com.mitchellh.ghostty" = {
     Unit = {
-      Description = "Ghostty Background Service";
-      After = [ "xdg-desktop-autostart.target" ];
+      Description = "Ghostty";
+      #After = [ "xdg-desktop-autostart.target" ];
+      After = [
+        "graphical-session.target"
+        "dbus.socket"
+      ];
+      Requires = [ "dbus.socket" ];
     };
 
     Service = {
+      #Type = "dbus";
+      Type = "notify-reload";
       BusName = "com.mitchellh.ghostty";
+      ReloadSignal = "SIGUSR2";
       ExecStart = "${ghostty} --gtk-single-instance=true --initial-window=false --quit-after-last-window-closed=false";
-      Type = "dbus";
     };
 
     Install = {
-      WantedBy = [ "xdg-desktop-autostart.target" ];
+      #WantedBy = [ "xdg-desktop-autostart.target" ];
+      WantedBy = [ "graphical-session.target" ];
     };
   };
 }
