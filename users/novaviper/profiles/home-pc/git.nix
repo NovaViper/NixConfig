95a7708606c16d824b72e8ff7acc15ec453f2d31
NovaViper <coder.nova99@mailbox.org>
2025-11-16 21:16:28 -0600

    git: properly fix the signing issues, break the signing stuff from .settings

--- users/novaviper/profiles/home-pc/git.nix
+++ users/novaviper/profiles/home-pc/git.nix
@@ -7,14 +7,16 @@ let
   hm-config = config.hm;
 in
 {
+  hm.programs.git.signing = {
+    format = "openpgp";
+    signByDefault = true;
+    key = "E5E6D90A268AC09D";
+  };
+
   hm.programs.git.settings = {
     user = {
       name = "NovaViper";
       email = myLib.utils.getUserVars "email" hm-config;
-    };
-    signing = {
-      key = "E5E6D90A268AC09D";
-      signByDefault = true;
     };
   };
 }
