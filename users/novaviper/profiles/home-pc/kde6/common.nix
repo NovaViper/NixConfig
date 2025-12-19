133e6a30da011806bc7298342177abee33b92600
NovaViper <coder.nova99@mailbox.org>
2025-12-19 12:40:54 -0600

    fish: actually fix reload-plasma-theme alias

--- users/novaviper/profiles/home-pc/kde6/common.nix
+++ users/novaviper/profiles/home-pc/kde6/common.nix
@@ -13,13 +13,7 @@ in
 
     "rm -R ~/.local/share/plasma-manager/last_run_script_* && ~/.local/share/plasma-manager/run_all.sh";
   hm.programs.fish.shellAliases.reload-plasma-theming = # fish
-    ''
-      set files ~/.local/share/plasma-manager/last_run_script_*
-      if test (count $files) -gt 0
-        rm -R $files
-      end
-      ~/.local/share/plasma-manager/run_all.sh
-    '';
+    "set files (${lib.getExe pkgs.findutils} ~/.local/share/plasma-manager/ -name 'last_run_*'); if test (count $files) -gt 0; rm -rf $files; end; ~/.local/share/plasma-manager/run_all.sh";
   hm.home.packages = [ pkgs.restart-plasma ];
 
   hm.programs.plasma = {
