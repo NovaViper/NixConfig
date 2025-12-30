bb5fff01b1cafdd48752dab34f918c423d64528b
NovaViper <coder.nova99@mailbox.org>
2025-12-29 18:49:00 -0600

    home: remove redundant backup flag

--- base/home-manager.nix
+++ base/home-manager.nix
@@ -21,7 +21,6 @@ in
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.backupFileExtension = ".bak";
   home-manager.extraSpecialArgs = {
     inherit
       self
