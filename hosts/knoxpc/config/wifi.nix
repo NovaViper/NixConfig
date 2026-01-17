9fbfc51df00533a7fc40e4db009f4d34e9f50d03
NovaViper <coder.nova99@mailbox.org>
2026-01-16 23:11:01 -0600

    knoxpc: add wifi network config
    For some reason recently it no longer used the local config.. so now
    making it declarative and hidden. Second try!

--- hosts/knoxpc/config/wifi.nix
+++ hosts/knoxpc/config/wifi.nix
@@ -1,4 +1,34 @@
-{ lib, ... }:
 {
+  config,
+  lib,
+  myLib,
+  inputs,
+  ...
+}:
+let
+  interfaces = [ "wlp2s0" ];
+in
+{
+  sops.secrets."server_networks" = myLib.secrets.mkSecretFile {
+    source = "networks.env";
+    format = "dotenv";
+    owner = "wpa_supplicant";
+    group = "wpa_supplicant";
+    restartUnits = map (id: "wpa_supplicant-${id}.service") interfaces;
+    subDir = [
+      "hosts"
+      "${config.networking.hostName}"
+    ];
+  };
+
   networking.wireless.enable = true;
+
+  networking.wireless.interfaces = interfaces;
+
+  networking.wireless.secretsFile = config.sops.secrets."server_networks".path;
+
+  networking.wireless.networks."${inputs.nix-secrets.networking.home-ssid}" = {
+    pskRaw = "ext:home_pskRaw";
+    authProtocols = [ "WPA-PSK" ];
+  };
 }
--- hosts/knoxpc/hardware-configuration.nix
+++ hosts/knoxpc/hardware-configuration.nix
@@ -33,5 +33,6 @@
   zramSwap.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.enableRedistributableFirmware = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
