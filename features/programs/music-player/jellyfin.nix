02a0bed067c4be3971c75b2e884e5aea6f27281c
NovaViper <coder.nova99@mailbox.org>
2026-01-17 19:35:03 -0600

    jellyfin: welcome back feishin!
    That package finally updated!

--- features/programs/music-player/jellyfin.nix
+++ features/programs/music-player/jellyfin.nix
@@ -5,27 +5,27 @@
 }:
 {
   hm.home.packages = with pkgs; [
-    # feishin
+    feishin
     jellyfin-tui
     pear-desktop
   ];
 
-  # hm.xdg.mimeApps = {
-  #   associations.added = {
-  #     "x-scheme-handler/ame" = "feishin.desktop";
-  #     "x-scheme-handler/feishin" = "feishin.desktop";
-  #     "x-scheme-handler/itms" = "feishin.desktop";
-  #     "x-scheme-handler/itmss" = "feishin.desktop";
-  #     "x-scheme-handler/musics" = "feishin.desktop";
-  #     "x-scheme-handler/music" = "feishin.desktop";
-  #   };
-  #   defaultApplications = {
-  #     "x-scheme-handler/ame" = "feishin.desktop";
-  #     "x-scheme-handler/feishin" = "feishin.desktop";
-  #     "x-scheme-handler/itms" = "feishin.desktop";
-  #     "x-scheme-handler/itmss" = "feishin.desktop";
-  #     "x-scheme-handler/musics" = "feishin.desktop";
-  #     "x-scheme-handler/music" = "feishin.desktop";
-  #   };
-  # };
+  hm.xdg.mimeApps = {
+    associations.added = {
+      "x-scheme-handler/ame" = "feishin.desktop";
+      "x-scheme-handler/feishin" = "feishin.desktop";
+      "x-scheme-handler/itms" = "feishin.desktop";
+      "x-scheme-handler/itmss" = "feishin.desktop";
+      "x-scheme-handler/musics" = "feishin.desktop";
+      "x-scheme-handler/music" = "feishin.desktop";
+    };
+    defaultApplications = {
+      "x-scheme-handler/ame" = "feishin.desktop";
+      "x-scheme-handler/feishin" = "feishin.desktop";
+      "x-scheme-handler/itms" = "feishin.desktop";
+      "x-scheme-handler/itmss" = "feishin.desktop";
+      "x-scheme-handler/musics" = "feishin.desktop";
+      "x-scheme-handler/music" = "feishin.desktop";
+    };
+  };
 }
