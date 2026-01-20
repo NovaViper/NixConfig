c2d70830f455d708683bba02266e1a2590fab6a3
NovaViper <coder.nova99@mailbox.org>
2026-01-19 20:21:43 -0600

    knox: add better host list for homepage

--- hosts/knoxpc/config/services/homepage/homepage.nix
+++ hosts/knoxpc/config/services/homepage/homepage.nix
@@ -1,10 +1,19 @@
-_: {
+{ config, ... }:
+let
+  mkHosts = addresses: builtins.concatStringsSep "," (map (addr: "${addr}:8082") addresses);
+in
+{
   services.glances.enable = true;
   services.homepage-dashboard = {
     enable = true;
     listenPort = 8082;
     openFirewall = true;
-    allowedHosts = "localhost:8082,127.0.0.1:8082,192.168.1.120:8082";
+    allowedHosts = mkHosts [
+      "localhost"
+      "127.0.0.1"
+      "192.168.1.120"
+      "${config.networking.hostName}"
+    ];
     settings = {
       tile = "KnoxPC Homelab";
       language = "en-US";
