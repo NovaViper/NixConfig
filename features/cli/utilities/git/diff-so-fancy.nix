a173f69467d12d4611641edfaf68b9e86945efbf
NovaViper <coder.nova99@mailbox.org>
2026-04-27 08:54:56 -0500

    refactor: force diff-so-fancy for pager.diff

--- features/cli/utilities/git/diff-so-fancy.nix
+++ features/cli/utilities/git/diff-so-fancy.nix
@@ -20,6 +20,6 @@ in
 
   hm.programs.git.iniContent = {
     # Auto-select the start of each diffed file, so `n` and `N` will go between them
-    pager.diff = "diff-so-fancy | less ${globalLessOpts} --pattern '^(Date|added|deleted|modified|renamed):'";
+    pager.diff = lib.mkForce "diff-so-fancy | less ${globalLessOpts} --pattern '^(Date|added|deleted|modified|renamed):'";
   };
 }
