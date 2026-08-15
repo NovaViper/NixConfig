edd7e4258c9e55b329ecbb15688480c66bbf1983
NovaViper <code4nova@aluwux.me>
2026-08-14 21:17:45 -0500

    refactor: use policies to get firefox extensions

--- config/core/firefox/extensions.nix
+++ config/core/firefox/extensions.nix
@@ -7,21 +7,107 @@
 let
   hm-config = config.hm;
 
-  extensions = with pkgs.inputs.firefox-addons; [
-    ublock-origin
-    sponsorblock
-    dearrow
-    return-youtube-dislikes
-    darkreader
-    #bypass-paywalls-clean
-    plasma-integration
-    indie-wiki-buddy
-    stylus
-    canvasblocker
-    steam-database
-  ];
+  moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+
+  # Extensions are obtained thanks to the guide here: https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265.
+  # Check `about:support` for extension/add-on ID strings. Then find the
+  # installation url by downloading the extension file (instead of installing it
+  # directly). Always install the latest version of the extensions by using the
+  # "latest" tag in the download url. Can also view the extensions source (Ctrl-U) and search for "guid"
+  # (quotations included) to get the ID string
+
+  # See also:
+  # - https://github.com/mozilla/policy-templates/blob/master/linux/policies.json#L120
+  # - https://mozilla.github.io/policy-templates/#extensionsettings
+  extensions = {
+    "uBlock0@raymondhill.net" = {
+      install_url = moz "ublock-origin";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+      private_browsing = true;
+    };
+
+    "sponsorBlocker@ajay.app" = {
+      install_url = moz "sponsorblock";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    "deArrow@ajay.app" = {
+      install_url = moz "dearrow";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    # Return Youtube Dislikes
+    "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+      install_url = moz "return-youtube-dislikes";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    "addon@darkreader.org" = {
+      install_url = moz "darkreader";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    # "bypass-paywalls-clean@codeberg.org" = {
+    #   install_url = moz "bypass-paywalls-clean";
+    #   installation_mode = "normal_installed";
+    # };
+
+    "plasma-browser-integration@kde.org" = {
+      install_url = moz "plasma-integration";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+    # Indie Wiki Buddy
+    "{cb31ec5d-c49a-4e5a-b240-16c767444f62}" = {
+      install_url = moz "indie-wiki-buddy";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    # Stylus
+    "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = {
+      install_url = moz "styl-us";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    "CanvasBlocker@kkapsner.de" = {
+      install_url = moz "canvasblocker";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    "steam-database@steamdb.info" = {
+      install_url = moz "steam-database";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    "simplelogin@simplelogin.io" = {
+      install_url = moz "simplelogin";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    "keepassxc-browser@keepassxc.org" = {
+      install_url = moz "keepassxc-browser";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+
+    "privacy@privacy.com" = {
+      install_url = moz "pay-by-privacy";
+      installation_mode = "normal_installed";
+      updates_disabled = true;
+    };
+  };
   extensionSettings = {
-    "keepassxc-browser@keepassxc.org".settings."settings" = {
+    "keepassxc-browser@keepassxc.org".settings.settings = {
       "colorTheme" = "system";
       defaultPasswordManager = true;
       autoReconnect = true;
@@ -32,17 +118,20 @@ let
       downloadFaviconAfterSave = true;
     };
   };
+
+  firefoxPolicies = {
+    ExtensionSettings = extensions // {
+      # Optional: prevent users from installing anything else.
+      # "*" = {
+      #   installation_mode = "blocked";
+      # };
+    };
+
+    "3rdparty".Extensions = extensionSettings;
+  };
 in
 {
-  hm.programs.floorp.profiles."${hm-config.home.username}".extensions = {
-    packages = extensions;
-    settings = extensionSettings;
-    force = true;
-  };
+  hm.programs.floorp.policies = firefoxPolicies;
 
-  hm.programs.firefox.profiles."${hm-config.home.username}".extensions = {
-    packages = extensions;
-    settings = extensionSettings;
-    force = true;
-  };
+  hm.programs.firefox.policies = firefoxPolicies;
 }
