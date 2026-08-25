bfbafc581c1148c55db5dfcf3eef8ff778178761
NovaViper <code4nova@aluwux.me>
2026-08-24 22:11:08 -0500

    chore: bump flake.lock
    - Also drop firefox-addons repo
    - Wanted to get latest version of vesktop and obs

--- flake.nix
+++ flake.nix
@@ -44,10 +44,6 @@
     #######
 
     # Extras
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
